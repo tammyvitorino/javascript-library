55c9ede164c4a124962f597bc46f26935dc5aa80
TammyVitorino <tamara@vitorino.net.br>
2023-01-24 07:15:00 +0000

    add: slide up function

--- useful-functions/animations.js
+++ useful-functions/animations.js
@@ -7,3 +7,14 @@ function fadeOut(element, opacity) {
     }
     element.style.opacity = opacity;
 }
+
+// Slide an element from bottom to its actual position
+function slideUp(element, time) {
+    element.animate([
+        { transform: 'translateY(100vh) scale(2)' },
+        { transform: 'translateY(0px) scale(1)' },
+    ], {
+        duration: time,
+        iterations: 1
+    });
+}
